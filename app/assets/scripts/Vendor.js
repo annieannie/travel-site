4bbbf2adacbc43144b36b396a87a1aa329f14c99
Design1 <annie01slc@gmail.com>
2018-05-09 12:22:07 -0600

    Added browser support fallback.

--- app/assets/scripts/Vendor.js
+++ app/assets/scripts/Vendor.js
@@ -1,3 +1,5 @@
+import '../../temp/scripts/modernizr.js';
 import 'lazysizes';
+import 'picturefill';
 
-console.log("hi there from vendor dot js");
+console.log("hi there from vendor dot js picturfill");
